4f1f167f5618633914101d2eba33eb13ddbbffcd
== <cc7474766@gmail.com>
2024-08-03 21:03:05 +0000

    temp convert

---
+++ Temperature_Converter.py
@@ -0,0 +1,9 @@
+# input temperature
+
+# output converted temperature
+
+""" PATH
+    learn how to make a terminal version of the temperatuer converter
+    
+    remake the temperature converter with tkinter as a gui
+"""
